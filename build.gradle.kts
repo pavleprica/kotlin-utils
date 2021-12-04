96ffe8dc94da001f6449b67648491a8c50f6d89a
Pavle Prica <pavle.prica5@gmail.com>
2021-12-04 19:33:22 +0100

    Add junit 5 and remove mainKt

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.test {
@@ -22,8 +25,4 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
